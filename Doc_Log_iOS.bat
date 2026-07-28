c1782613b77023f4c17ee376b64ce6ae283fcd75
DeerXua <DeerXua@users.noreply.github.com>
2026-07-28 17:38:34 +0700

    Filter strictly for DXMOD_SKIN, DXMOD_SKIN_SYNC and WeaponSkin_Special logs

--- Doc_Log_iOS.bat
+++ Doc_Log_iOS.bat
@@ -11,8 +11,10 @@ echo   Dang ket noi toi thiet bi iOS qua USB...
 echo   (Luu y: Vui long mo khoa iPhone va chon "Tin cay may tinh nay")
 echo --------------------------------------------------------
 echo.
-"C:\ExtractedPak\TOOL PAK DX\Tools\libimobiledevice\idevicesyslog.exe" | findstr /i "DXMOD DXMOD_SKIN DXMOD_SKIN_SYNC"
+"C:\ExtractedPak\TOOL PAK DX\Tools\libimobiledevice\idevicesyslog.exe" | findstr /i "DXMOD_SKIN DXMOD_SKIN_SYNC WeaponSkin_Special"
 pause
+
+
 
 
 
